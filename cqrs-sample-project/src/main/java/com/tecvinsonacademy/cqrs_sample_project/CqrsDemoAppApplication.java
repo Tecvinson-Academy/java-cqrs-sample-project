aa4a75bbc9cbafce2cce64e093358822bf3e9b77
adesola oladejo <sola@bluechipisolutions.com>
2024-11-30 14:28:03 +0100

    project setup

---
+++ cqrs-sample-project/src/main/java/com/tecvinsonacademy/cqrs_sample_project/CqrsDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.tecvinsonacademy.cqrs_sample_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsDemoAppApplication.class, args);
+	}
+
+}
---
+++ cqrs-sample-project/src/test/java/com/tecvinsonacademy/cqrs_sample_project/CqrsDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecvinsonacademy.cqrs_sample_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
